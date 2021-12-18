4af38863a6512fc9d0aebe0b14ba118570300c9e
Oryto Mikli <orytomikli21@gmail.com>
2021-12-19 00:28:31 +0700

    some learn

---
+++ some/ls.js
@@ -0,0 +1,33 @@
+// let mario = "Wellcome to the page!";
+// let names = 'Oryto';
+// let age = 19;
+// console.log(mario, '\nYour name is:', names.toLocaleUpperCase().slice(1,3), '\nYour name lenght is:', names.length );
+// if (age < 18){
+//   console.log(mario, "\nYou're only ", age);
+// } else {
+//   console.log("You're Allowed because you're 18+")
+// };
+// JUST LEARN CONTROL FLOW AND VARIABLES
+
+
+// NOW LETS DO THE MATH MAGIC
+
+//LET'S FIND THE AREA OF A CIRCLE 
+
+// const radius = 77;
+// const pi = 3.14;
+// const area_of_circle = pi*(radius**2);
+// let result = 'Area of the circle with ${radius} radius is: ${area_of_circle}';
+
+// console.log(result);
+
+
+// let html = '
+//  <h2>${title}</h2>
+
+// ';
+
+// let hastag = 9;
+// for(let i = 1; i < hastag; i++){
+//   console.log('#' * i + '</br>');
+// }
